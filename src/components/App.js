f69676cb0279ded4ce152e806ce17dfac53b1a64
Thuyen <phantrongthuyen9d@gmail.com>
2018-12-01 17:29:30 +0700

    new main-layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Wall from "./Wall";
+import Navbar from "./Navbar";
 class App extends Component {
   render() {
     return (
       <div>
+
         <Wall/>
       </div>
     );
