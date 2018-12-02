0bf4dd8b04f44ff41277bc26e81598b3772b5df9
JBThong <qktc1422@gmail.com>
2018-12-02 17:12:15 +0700

    add url in store

--- src/reducers/userInfo.js
+++ src/reducers/userInfo.js
@@ -2,6 +2,8 @@ const initState = {
     displayName: 'Nguyễn Đình Tiến',
     userName:'@Nguynnh74289770',
     joinTime: 1543641338,
+    urlPhotoCover: "https://pbs.twimg.com/profile_banners/173407308/1405769923/1500x500",
+    urlAvatar:"https://pbs.twimg.com/profile_images/552490918989668352/ywlPHVTJ_400x400.jpeg",
     other:{
         tweetCount: 10,
         followingCount: 2,
