317f7e7d98b8e919968262b3a16c16023aa9552f
JBThong <qktc1422@gmail.com>
2018-12-02 11:54:55 +0700

    add container for userprofile

---
+++ src/containers/user-profile.js
@@ -0,0 +1,16 @@
+import {connect} from 'react-redux';
+import UserProfile from "../components/UserProfile";
+
+const mapStateToProps = state => {
+	return {
+		userInfo: state.userInfo
+	}
+}
+
+
+const mapDispatchToProps = dispatch => {
+    return {}
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
