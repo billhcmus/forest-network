c5f053b7ea28095a8af67b1a5b9871bd83bf9058
JBThong <qktc1422@gmail.com>
2018-12-04 03:02:13 +0700

    remove

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { BrowserRouter as Link } from 'react-router-dom';
-
 
 class UserProfile extends Component {
     
