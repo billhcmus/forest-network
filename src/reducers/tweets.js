33dc8a09475fb1c97f96a90a46a844e84d7c0484
Thuyen <phantrongthuyen9d@gmail.com>
2018-12-03 10:40:40 +0700

    fix scroll

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -47,6 +47,34 @@ const initState = {
                     content: "Lorens"
                 }
             ]
+        },
+        {
+            id:2,
+            urlAvatar: "https://pbs.twimg.com/profile_images/552490918989668352/ywlPHVTJ_400x400.jpeg",
+            author:'@Nguynnh74289770',
+            displayName: "JBThong",
+            time:15436414000,
+            content:'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, ' +
+                'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, ' +
+                'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR ' +
+                'COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF ' +
+                'CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE ' +
+                'OR OTHER DEALINGS IN THE SOFTWARE.',
+            likesCount:10,
+            comments: [
+                {
+                    id: 1,
+                    content: "Lorens"
+                },
+                {
+                    id: 2,
+                    content: "Lorens"
+                },
+                {
+                    id: 3,
+                    content: "Lorens"
+                }
+            ]
         }
     ]
 };
