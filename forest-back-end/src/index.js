c0fb1850b9fa7fb68e88617e13508e2c1114c469
Thuyen <phantrongthuyen9d@gmail.com>
2018-12-09 00:11:54 +0700

    create account, make payment

--- forest-back-end/src/index.js
+++ forest-back-end/src/index.js
@@ -8,6 +8,9 @@ import AppRouter from './router/app-router';
 import Model from './models';
 import DataBase from './database';
 import {decode} from './transaction'
+const vstruct = require('varstruct');
+const {Keypair} = require('stellar-base');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -33,12 +36,15 @@ app.models = new Model(app);
 // });
 
 // Test decode transaction
-let base64string = 'ATAdxPY3gelauaEcMyjMIuaJsiFe9bIO5kZTCil6jcMzJAwoAAAAAAAAADoQQ1RUNTIyLUNRMjAxNS8zMgEAIzDhU67FhbbPH1tOkMiwKuMPnuS98tNm/s27wMjNfWQfUT1L5St7LG3JTvNxp4fSxSsJWBVfOKJF3ptyjIHen8deWoBr+QlyXd9kKwBfBPjlDbllD9Y1Xa8zUI5OMIS9zmn0AQ==';
-let buf = new Buffer(base64string, 'base64');
+// let base64string = 'ATA8i2kusx/LGR5eAZ/7L2FhJIL8wtK3QdYou1L0XkGbXSLfAAAAAAAAAAEAAQAjMB3E9jeB6Vq5oRwzKMwi5omyIV71sg7mRlMKKXqNwzMkDCjZtz7CjPZuCL8TmGDO2fjIH8UcLxNYBM2KkRb/l89C7vMg3wITL2dmGwQXihie7bgL20r1i5yu+6PO//p89GUJ';
+// let buf = new Buffer(base64string, 'base64');
 
-let tx = decode(buf);
+// let tx = decode(buf);
 
-console.log(tx);
+// console.log(tx);
+
+app.models.user.makePayment();
+
 
 server.listen(process.env.PORT || PORT, () => {
     console.log(`App is running on port ${server.address().port}`)
--- forest-back-end/src/models/user.js
+++ forest-back-end/src/models/user.js
@@ -1,7 +1,12 @@
+const {Keypair} = require('stellar-base');
+import {encode, decode, verify, sign, hash} from '../transaction'
+
 export default class User {
     constructor(app) {
         this.app = app;
         this.UserSayHello = this.UserSayHello.bind(this);
+        this.createAccount = this.createAccount.bind(this);
+        this.makePayment = this.makePayment.bind(this);
     }
 
 
@@ -9,5 +14,43 @@ export default class User {
         return new Promise((resolve, reject) => {
             resolve("Hello from User Model");
         })
+    }
+
+    createAccount() {
+        // 
+        const key = Keypair.random();
+        let secretKey = key.secret();
+        let publicKey = key.publicKey();
+        
+        let tx = {
+            version: 1,
+            account: '',
+            sequence: 1,
+            memo: Buffer.alloc(0),
+            operation: 'create_account',
+            params: {address: publicKey},
+            signature: Buffer.alloc(64, 0)
+        }
+
+        sign(tx, secretKey);
+    }
+
+    makePayment() {
+        let address = 'GDMZJFJVTR4PWYGZJEHN2USXQSEXNKET4AWDIUNJX7ZE56PUCTEY5NOO';
+        const key = Keypair.random();
+        let secretKey = key.secret();
+        let publicKey = key.publicKey();
+        let tx = {
+            version: 1,
+            account: '',
+            sequence: 1,
+            memo: Buffer.alloc(0),
+            operation: 'create_account',
+            params: {address: address, amount: 10000},
+            signature: Buffer.alloc(64, 0)
+        }
+
+        sign(tx, secretKey)
+        console.log(hash(tx));
     }
 }
